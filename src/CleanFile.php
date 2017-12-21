5a917080b392d179038b40573b455a1fa2311f33
Wu Nan <904215764@qq.com>
2017-12-21 09:42:59 +0800

    Create CleanFile.php

---
+++ src/CleanFile.php
@@ -0,0 +1,29 @@
+<?php
+namespace Spexial\Iwunan;
+class CleanFile
+{
+    public function Clean($dir)
+    {
+	$a = array();
+        if (is_dir($dir)){
+            if ($dh = opendir($dir)){
+                while (($file = readdir($dh))!== false){
+                    $filePath = $file;
+                    if($filePath!="." && $filePath!="..") {
+                        $a [] = $filePath;
+                    }
+                }
+                closedir($dh);
+            }
+        }
+        $b = DB::table('data')->lists('path');
+        foreach($a as $key => $value) {
+            if(!in_array($value,$b)) {
+                unlink($dir.$value);
+                echo "数据库不存在".$value."，已删除...<br/>";
+            }
+        }
+        $msg = json_encode(['code'=>1,'msg'=>'success','content'=>'Method completed.']);
+        return $msg;
+    }
+}

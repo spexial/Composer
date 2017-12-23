7d7495577b13d4f9b628af9cf3bfdaa12d77f02b
Wu Nan <904215764@qq.com>
2017-12-23 15:18:30 +0800

    Create Regex.php

---
+++ src/Regex.php
@@ -0,0 +1,22 @@
+<?php
+namespace Spexial\Iwunan;
+class Regex
+{
+    /**
+     * 判断输入的网站格式是否正确
+     * @param $web
+     * @return bool|string
+     */
+    public function inputUrl($web)
+    {
+        if(preg_match('/(http:\/\/|https:\/\/)+www.[0-9a-zA-Z]+_?[0-9a-zA-Z]+.[0-9a-zA-Z]{2,}$/',$url)) {
+            return  $web;
+        }elseif(preg_match('/^www.[0-9a-zA-Z]+_?[0-9a-zA-Z]+.[0-9a-zA-Z]{2,}$/',$url)){
+            return 'http://'.$web;
+        }elseif(preg_match('/[0-9a-zA-Z]+_?[0-9a-zA-Z]+.[0-9a-zA-Z]{2,}$/',$url)){
+            return 'http://www.'.$web;
+        }else{
+            return false;
+        }
+    }
+}
